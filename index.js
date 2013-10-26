f074e996e4138f602111a8d013ed9fda3ead135e
Ivan Vergiliev <ivan@soundcloud.com>
2013-10-26 11:53:20 +0300

    Add package.json and static file serving.

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var http = require('http');
+var WebSocketServer = require('ws').Server;
+
+var app = express();
+app.use(express.static(__dirname + '/static'));
+
+var server = http.createServer(app);
+server.listen(3000);
+
+var wss = new WebSocketServer({server: server});
+wss.on('connection', function (ws) {
+  ws.on('message', function dispatch() {
+    
+  });
+});
